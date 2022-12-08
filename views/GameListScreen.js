a6ed786b8437c2c33cce3f16757d8a53e2286932
macbook m1 pro <yanisjdz@outlook.fr>
2022-12-08 12:06:13 +0100

    styling card

--- views/GameListScreen.js
+++ views/GameListScreen.js
@@ -27,7 +27,7 @@ export default GameListScreen = ({}) => {
       await axios
         .get('https://www.freetogame.com/api/games?sort-by=release-date')
         .then(response => {
-            setLastGames([...lastGames, ...response.data.slice(0, 20)]);          
+          setLastGames([...lastGames, ...response.data.slice(0, 20)]);
         });
     } catch (error) {
       console.error(error);
@@ -39,7 +39,29 @@ export default GameListScreen = ({}) => {
       <FlatList
         data={lastGames}
         renderItem={({item, index}) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View key={index} style={styles.card}>
+              <View style={styles.cardImage}>
+                <Image
+                  source={{
+                    uri: item.thumbnail,
+                  }}
+                  style={styles.cardImage}
+                  resizeMode="cover"
+                />
+              </View>
+              <View style={styles.cardInfos}>
+                <View style={styles.cardDetails}>
+                  <Text style={styles.cardTitle} numberOfLines={1} ellipsizeMode='tail'>{item.title}</Text>
+                  <Text style={styles.cardTitle} numberOfLines={1} ellipsizeMode='tail'>{item.genre}</Text>
+                </View>
+                <View style={styles.cardDetails}>
+                  <Text style={styles.cardTitle} numberOfLines={1} ellipsizeMode='tail'>{item.title}</Text>
+                  <Text style={styles.cardTitle} numberOfLines={1} ellipsizeMode='tail'>{item.genre}</Text>
+                </View>
+              </View>
+            </View>
+          );
         }}
       />
     </SafeAreaView>
@@ -48,6 +70,38 @@ export default GameListScreen = ({}) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    padding: 20,
   },
+  card: {
+    flex: 1,
+    flexDirection: 'row',
+
+    marginVertical: 10,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+
+    height: 100,
+    borderRadius: 15,
+    backgroundColor: '#dfe4ea',
+    alignItems: 'center',
+  },
+  cardImage: {
+    width: 80,
+    height: 70,
+    borderRadius: 10,
+  },
+  cardInfos: {
+    flex: 1,
+    flexDirection: 'column',
+    marginLeft: 10,
+  },
+  cardDetails: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  cardTitle:{
+    width: 150,
+    overflow:'hidden',
+  }
 });
