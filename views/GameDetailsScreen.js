310ba0aa9d4f2d465ccd5c299f7cf7fb1aeee55c
macbook m1 pro <yanisjdz@outlook.fr>
2022-12-08 18:48:37 +0100

    items to display

--- views/GameDetailsScreen.js
+++ views/GameDetailsScreen.js
@@ -18,17 +18,21 @@ export default GameDetailsScreen = ({item}) => {
   return (
     <SafeAreaView>
       <View>
-        <Text>item.title</Text>
-        <Text>item.thumbnail</Text>
-        <Text>item.short_description</Text>
-        <Text>item.genre</Text>
-        <Text>item.platform</Text>
-        <Text>item.publisher</Text>
-        <Text>item.release_date</Text>
-        <TouchableOpacity style={{padding: 10, backgroundColor:'#70a1ff', height: 40, width: 150}}>
-            <Text>
-                item.game_url
-            </Text>
+        <Text>{item.title}</Text>
+        <Text>{item.thumbnail}</Text>
+        <Text>{item.short_description}</Text>
+        <Text>{item.genre}</Text>
+        <Text>{item.platform}</Text>
+        <Text>{item.publisher}</Text>
+        <Text>{item.release_date}</Text>
+        <TouchableOpacity
+          style={{
+            padding: 10,
+            backgroundColor: '#70a1ff',
+            height: 40,
+            width: 150,
+          }}>
+          <Text>{item.game_url} </Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
